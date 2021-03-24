13414a77756c31a766cf0f36d3e450423ec94df9
douglasblnk <douglasblank1@gmail.com>
2021-03-24 07:33:35 -0300

    chore: improved code styles

--- serverless-voice-memo/src/components/audio-dots/AudioDots.tsx
+++ serverless-voice-memo/src/components/audio-dots/AudioDots.tsx
@@ -11,10 +11,16 @@ export default function AudioDots() {
     [0,1,2,3,4,5,6],
     [0,1,2,3,4,5,6],
   ].map((dots, index) =>
-    <div key={`wrapper-${index.toString()}`} className={styles.dotsCol}>
+    <div
+      key={`wrapper-${index.toString()}`}
+      className={styles.dotsCol}
+    >
       {
         dots.map(number =>
-          <div key={`dot-${number.toString()}`} className={styles.dotsDot} />
+          <div
+            key={`dot-${number.toString()}`}
+            className={styles.dotsDot}
+          />
         )
       }
     </div>
