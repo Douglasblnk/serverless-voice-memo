442a1ccc81ba122259e9048b13537f6f965b2c69
douglasblnk <douglasblank1@gmail.com>
2021-03-28 23:28:49 -0300

    feat: removed all old vanilla code and ts code

--- serverless-voice-memo/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const getScale = (buttonWidth, buttonHeight) => {
-  const vw = window.innerWidth;
-  const vh = window.innerHeight;
-
-  if (vw > vh)
-    return (`scale(${(vw / buttonWidth?.offsetWidth) * 1.5}) translate(-50%,-50%)`);
-
-  return (`scale(${(vh / buttonHeight?.offsetWidth) * 1.5}) translate(-50%,-50%)`);
-};
