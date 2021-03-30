2b7678714e00165a19795b1af43a6dedeb4031d9
douglasblnk <douglasblank1@gmail.com>
2021-03-30 07:38:33 -0300

    feat: implemented the new musicBeat component

--- app/src/components/audio-list/AudioList.jsx
+++ app/src/components/audio-list/AudioList.jsx
@@ -1,17 +1,17 @@
 import styles from './AudioList.module.scss';
 
 import AudioPlayer from '../audio-player/AudioPlayer';
+import MusicBeat from '../music-beat/MusicBeat';
 
 export default function AudioList({ recordedBlobs }) {
   return (
     <div className={styles.audioListContainer}>
       {
         recordedBlobs.map((audioBlob, index) => (
-          <div key={`audioBlob-${index}`}>
+          <div key={`audioBlob-${index}`} style={{ display: 'flex', alignItems: 'center' }}>
             Audio Blob { audioBlob.toString() }
-            Audio Blob { audioBlob.toString() }
-            Audio Blob { audioBlob.toString() }
-            Audio Blob { audioBlob.toString() }
+
+            <MusicBeat />
           </div>
         ))
       }
---
+++ app/src/components/music-beat/MusicBeat.jsx
@@ -0,0 +1,15 @@
+import styles from './MusicBeat.module.scss'
+
+export default function MusicBeat() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.line1} />
+      <div className={styles.line2} />
+      <div className={styles.line3} />
+      <div className={styles.line4} />
+      <div className={styles.line5} />
+      <div className={styles.line6} />
+      <div className={styles.line7} />
+    </div>
+  )
+}
