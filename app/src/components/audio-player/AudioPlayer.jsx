0566bca2092343fbf7c5b90cc63b77ae9769b283
douglasblnk <douglasblank1@gmail.com>
2021-03-29 23:48:23 -0300

    feat: initial setup of the new audio player component

---
+++ app/src/components/audio-player/AudioPlayer.jsx
@@ -0,0 +1,9 @@
+import styles from './AudioPlayer.module.scss'
+
+export default function AudioPlayer() {
+  return (
+    <div className={styles.audioPlayerContainer}>
+      Audio player
+    </div>
+  )
+}
