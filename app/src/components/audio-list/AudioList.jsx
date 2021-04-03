687970cf74a7bb5b117e52f78e246220ff5f3641
douglasblnk <douglasblank1@gmail.com>
2021-04-03 15:04:21 -0300

    feat: added class to audioList and new props

--- app/src/components/audio-list/AudioList.jsx
+++ app/src/components/audio-list/AudioList.jsx
@@ -2,16 +2,17 @@ import styles from './AudioList.module.scss';
 
 import AudioPlayer from '../audio-player/AudioPlayer';
 
-export default function AudioList({ recordedBlobs, audioSrc }) {
+export default function AudioList({ recordedBlobs, removeAudio }) {
   return (
     <div className={styles.audioListContainer}>
       {
         recordedBlobs.map((audioBlob, index) => (
-          <div key={`audioBlob-${index}`}>
+          <div key={`audioBlob-${index}`} className={styles.audioList}>
             <AudioPlayer
               audioBlob={audioBlob}
-              audioSrc={audioSrc}
               audioTitle={`Audio ${index + 1}`}
+              audioIndex={index}
+              removeAudio={removeAudio}
             />
           </div>
         ))
